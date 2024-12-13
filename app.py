50eddad3e5228fb85602b3c40c61f9cd854521be
Eugene Custodio <139606244+eugenecustodio@users.noreply.github.com>
2024-12-13 13:54:22 +0800

    Update app.py sadasd

--- app.py
+++ app.py
@@ -145,6 +145,7 @@ def predict_url():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 10000))  # Use Render's default port or fallback to 10000
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
