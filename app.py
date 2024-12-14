9c47f97720b32eccd44bd51ddbd61405bf2024d7
Eugene Custodio <139606244+eugenecustodio@users.noreply.github.com>
2024-12-14 10:38:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -144,8 +144,5 @@ def predict_url():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Use Render's default port or fallback to 10000
-    app.run(host="0.0.0.0", port=port, debug=True)
 
 
